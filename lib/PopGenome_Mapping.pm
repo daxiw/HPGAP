0d71f24a7bb191d7c78b188fa1785b54471ba6ac
Daxi Wang <daxiw1@student.unimelb.edu.au>
2019-10-12 09:19:39 +0800

    Prepare_yml.pl

--- lib/PopGenome_Mapping.pm
+++ lib/PopGenome_Mapping.pm
@@ -36,33 +36,46 @@ sub MAPPING{
 			open SH, ">$shpath/$sample.step1b.sh";		
 			print SH "#!/bin/sh\ncd $sample_outpath\n";
 			foreach my $lib (keys %{$samplelist{$sample}{rawdata}}){
-				print SH "bwa mem $reference ../../read_filtering/$sample/$lib\_1.filt.fq.gz ../../read_filtering/$sample/$lib\_2.filt.fq.gz -t $cfg{args}{threads} -R \"\@RG\\tID:$lib\\tSM:$sample\\tLB:$lib\\tPL:$samplelist{$sample}{rawdata}{$lib}{PL}\"\| samtools view -bS -@ $cfg{args}{threads} -F 4 - -o $lib\_filt.bam && \\\n" if($samplelist{$sample}{rawdata}{$lib}{Flag} eq "PE");
-				print SH "bwa mem $reference ../../read_filtering/$sample/$lib\_1.filt.fq.gz -t $cfg{args}{threads} -R \"\@RG\\tID:$lib\\tSM:$sample\\tLB:$lib\\tPL:$samplelist{$sample}{rawdata}{$lib}{PL}\"\| samtools view -bS -@ 10 -F 4 - -o $lib\_filt.bam && \\\n" if($samplelist{$sample}{rawdata}{$lib}{Flag} eq "SE");
+				if($samplelist{$sample}{rawdata}{$lib}{Flag} eq "PE"){
+					print SH "bwa mem $reference ../../read_filtering/$sample/$lib\_1.filt.fq.gz ../../read_filtering/$sample/$lib\_2.filt.fq.gz -t $cfg{args}{threads} -R \"\@RG\\tID:$lib\\tSM:$sample\\tLB:$lib\\tPL:$samplelist{$sample}{rawdata}{$lib}{PL}\"\| samtools view -bS -@ $cfg{args}{threads} -F 4 - -o $lib\_filt.bam && \\\n";
+				}
+				elsif($samplelist{$sample}{rawdata}{$lib}{Flag} eq "SE"){
+					print SH "bwa mem $reference ../../read_filtering/$sample/$lib\_1.filt.fq.gz -t $cfg{args}{threads} -R \"\@RG\\tID:$lib\\tSM:$sample\\tLB:$lib\\tPL:$samplelist{$sample}{rawdata}{$lib}{PL}\"\| samtools view -bS -@ 10 -F 4 - -o $lib\_filt.bam && \\\n";
+				}
+				#summarise statistics for each library bam file 
+				print SH "samtools stats -@ $cfg{args}{threads} $lib\_filt.bam 1>$lib\_filt.bamstat.txt 2>$lib\_filt.bamstat.txt.e && echo \"** $lib\_filt.bamstat.txt done **\"\n";
+				#then sort each library bam file 
 				print SH "samtools sort -@ $cfg{args}{threads} $lib\_filt.bam -o $lib\_filt.sort.bam --output-fmt BAM && \\\n";
+				#then remove the unsorted bam file
 				print SH "rm -f $lib\_filt.bam\n";
 			}
 
+			#when there is only one library/lane for each sample
 			if (keys %{$samplelist{$sample}{rawdata}} == 1){
-				foreach my $lib (keys %{$samplelist{$sample}{rawdata}}){print SH "mv $lib\_filt.sort.bam $sample.sorted.bam\n";}
+				foreach my $lib (keys %{$samplelist{$sample}{rawdata}}){
+					print SH "mv $lib\_filt.sort.bam $sample.sorted.bam\n";}
 				print SH "samtools stats -@ $cfg{args}{threads} $sample.sorted.bam 1>bam.stats.txt 2>bam.stats.txt.e && echo \"** bam.stats.txt done **\"\n";
 			}
 
+			#when there is more than one library/lane for each sample
 			if (keys %{$samplelist{$sample}{rawdata}} > 1){
-				print SH "samtools merge -nr -@ $cfg{args}{threads} $sample.bam *_filt.sort.bam && echo \"** $sample.sort.bam done **\" && rm -f *_filt.sort.bam\n";
-				print SH "samtools sort -@ $cfg{args}{threads} $sample.bam -o $sample.sorted.bam --output-fmt BAM && echo \"** $sample.sorted.bam done **\" && rm -f $sample.bam\n";
+				print SH "samtools merge -nr -@ $cfg{args}{threads} $sample.sorted.bam *_filt.sort.bam && echo \"** $sample.sort.bam done **\" && rm -f *_filt.sort.bam\n";
+				#print SH "samtools sort -@ $cfg{args}{threads} $sample.bam -o $sample.sorted.bam --output-fmt BAM && echo \"** $sample.sorted.bam done **\" && rm -f $sample.bam\n";
 				print SH "samtools stats -@ $cfg{args}{threads} $sample.sorted.bam 1>bam.stats.txt 2>bam.stats.txt.e && echo \"** bam.stats.txt done **\"\n";
 			}
 			close SH;
 			print CL "sh $shpath/$sample.step1b.sh 1>$shpath/$sample.step1b.sh.o 2>$shpath/$sample.step1b.sh.e \n";
 		}
 		close CL;
-		my $threads = 1+$cfg{args}{threads}/2;
+		my $threads = $cfg{args}{threads};
 		`perl $Bin/lib/qsub.pl -d $shpath/cmd_step1b_qsub -q $cfg{args}{queue} -P $cfg{args}{prj} -l 'vf=$cfg{args}{mem},num_proc=$threads -binding linear:1' -m 100 -r $shpath/cmd_step1b.list` unless ($skipsh ==1);
 	}
 
 	# create this yaml object
 	$yaml = YAML::Tiny->new( \%cfg );
 	# Save both documents to a file
+	$yml_file = s/\.yml$//g;
+	$yml_file = $yml_file."_mapping_modified.yml";
 	$yaml->write( $yml_file );
 }
 
