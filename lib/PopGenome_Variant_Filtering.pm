8963924c51a4299f7f4d83068391057d0566c6b7
Daxi Wang <daxiw1@student.unimelb.edu.au>
2019-11-28 15:55:20 +0800

    Nov2019

--- lib/PopGenome_Variant_Filtering.pm
+++ lib/PopGenome_Variant_Filtering.pm
@@ -20,9 +20,10 @@ sub Main{
 		'overwrite',
 		'allsteps',
 		'threads',
-		'variant_filtering',
+		'gatk_filtering',
 		'freebayes_filtering',
-#		'intersection',
+		'intersection',
+		'advanced_filtering',
 		'help',
 		'skipsh');
 
@@ -62,6 +63,7 @@ sub Main{
 
 	if (defined $opts{variant_filtering}){ &VariantFiltering (\%var,\%opts);}
 	if (defined $opts{freebayes_filtering}){ &FreebayesBasicFiltering (\%var,\%opts);}
+	if (defined $opts{advanced_filtering}){ &AdvancedFiltering (\%var,\%opts);}
 #	if (defined $opts{intersection}){ &ReadReport (\%var,\%opts);}
 }
 
@@ -138,23 +140,20 @@ sub FreebayesBasicFiltering {
 	`perl $Bin/lib/qsub.pl -d $var{shpath}/freebayes_basic_filtering_qsub -q $cfg{args}{queue} -P $cfg{args}{prj} -l 'vf=1G,num_proc=2 -binding linear:1' -m 100 -r $var{shpath}/cmd_freebayes_basic_filtering.list` unless (defined $opts{skipsh});
 }
 
-sub FinalFiltering {
+sub AdvancedFiltering {
 	my ($var,$opts) = @_;
 	my %opts = %{$opts};
 	my %var = %{$var};
 	my %cfg = %{$var{cfg}};
 	my %samplelist = %{$var{samplelist}};
 
-	open CL, ">$var{shpath}/cmd_variant_filtering_s1.list";
-	open SH, ">$var{shpath}/variant_filtering_s1.sh";
-
-	#print SH "vcftools --vcf $var{outpath}/../JointCalling/JointCalling_filtered_snps1st.vcf --remove-filtered-all --recode --recode-INFO-all --stdout \| bgzip -c > $var{outpath}/PASS.SNP.vcf.gz && echo \"** finish variant_filtering_s1 **\" > $var{shpath}/variant_filtering_s1.finished.txt \n";
-	
+	#print SH "vcftools --gzvcf freebayes_joint_calling_filtered1.vcf.gz --max-alleles 2 --remove-filtered-all --recode --recode-INFO-all --stdout \| vcfsnps\|bgzip -c > $var{outpath}/basic_snp.vcf.gz \n";
+	`cp -f $var{outpath}/freebayes_joint_calling_filtered1.vcf.gz $var{outpath}/basic_snp.vcf.gz`;
 	my $genome=PopGenome_Shared::LOADREF($var{reference});
 
 	###filter SNP by depth if needed
 	my $sample_size = keys %{$cfg{fqdata}};
-	open (IN, "zcat $var{outpath}/PASS.SNP.vcf.gz|") or die $!;
+	open (IN, "zcat $var{outpath}/basic_snp.vcf.gz|") or die $!;
 	my @dp;
 	while (<IN>){
     	next if (/#/);
@@ -173,23 +172,23 @@ sub FinalFiltering {
 		}
 	}
 
+	open CL, ">$var{shpath}/cmd_variant_filtering_s1.list";
+	open SH, ">$var{shpath}/variant_filtering_s1.sh";
 	#default SNV sites 1: PASS + max-meanDP: 3*$avgdp50 + max-missing: 0.8 + biallelic + selected chr (optional)
-	print SH "vcftools --gzvcf $var{outpath}/PASS.SNP.vcf.gz $chrcmd --min-meanDP 1 --max-meanDP $avgdp50 --max-missing 0.8 --max-alleles 2 --remove-filtered-all --recode --recode-INFO-all --stdout \| bgzip -c > $var{outpath}/PASS.SNP.DP.vcf.gz && echo \"** finish variant_filtering_s2 **\" > $var{shpath}/variant_filtering_s2.finished.txt\n";
+	print SH "vcftools --gzvcf $var{outpath}/basic_snp.vcf.gz $chrcmd --min-meanDP 5 --max-meanDP $avgdp50 --max-missing 0.8 --max-alleles 2 --remove-filtered-all --recode --recode-INFO-all --stdout \| bgzip -c > $var{outpath}/basic_snp_dp.vcf.gz \n";
 	#switch on the bash running
 	print CL "sh $var{shpath}/variant_filtering_s1.sh 1>$var{shpath}/variant_filtering_s1.sh.o 2>$var{shpath}/variant_filtering_s1.sh.e\n";
 	close CL;
 	`perl $Bin/lib/qsub.pl -d $var{shpath}/variant_filtering_s1_qsub -q $cfg{args}{queue} -P $cfg{args}{prj} -l 'vf=1G,num_proc=2 -binding linear:1' -m 100 -r $var{shpath}/cmd_variant_filtering_s1.list` unless (defined $opts{skipsh});
-	
 
-	$cfg{step1}{variant_filtering}{vcf}="$var{outpath}/PASS.SNP.DP.vcf.gz";
-
+	$cfg{variant_filtering}{basic_vcf}="$var{outpath}/basic_snp_dp.vcf.gz";
 	### prepare the setting for low LD prunning
-	$cfg{step1}{variant_filtering}{scaffold_number_limit} = 95 unless (defined $cfg{step1}{variant_filtering}{scaffold_number_limit});
-	$cfg{step1}{variant_filtering}{scaffold_length_cutoff} = 0 unless (defined $cfg{step1}{variant_filtering}{scaffold_length_cutoff});
+	$cfg{variant_filtering}{scaffold_number_limit} = 95 unless (defined $cfg{variant_filtering}{scaffold_number_limit});
+	$cfg{variant_filtering}{scaffold_length_cutoff} = 0 unless (defined $cfg{variant_filtering}{scaffold_length_cutoff});
 	#set ld prunning cut off
-	$cfg{step1}{variant_filtering}{ldwindowsize} = 10 unless (defined $cfg{step1}{variant_filtering}{ldwindowsize});
-	$cfg{step1}{variant_filtering}{ldwindowstep} = 5 unless (defined $cfg{step1}{variant_filtering}{ldwindowstep});
-	$cfg{step1}{variant_filtering}{ldcutoff} = 0.3 unless (defined $cfg{step1}{variant_filtering}{ldcutoff});
+	$cfg{variant_filtering}{ldwindowsize} = 10 unless (defined $cfg{variant_filtering}{ldwindowsize});
+	$cfg{variant_filtering}{ldwindowstep} = 5 unless (defined $cfg{variant_filtering}{ldwindowstep});
+	$cfg{variant_filtering}{ldcutoff} = 0.3 unless (defined $cfg{variant_filtering}{ldcutoff});
 
 	### load reference and map scaffold to number list 
 	my $i=1;my $j=0;
@@ -197,49 +196,47 @@ sub FinalFiltering {
 	foreach my $id(sort { $genome->{len}{$b} <=> $genome->{len}{$a} } keys %{$genome->{len}}){
 		print OT "$id\t$i\n";
 		$i++;
-		if (($genome->{len}{$id} >= $cfg{step1}{variant_filtering}{scaffold_length_cutoff})&&($j < $cfg{step1}{variant_filtering}{scaffold_number_limit})){
+		if (($genome->{len}{$id} >= $cfg{variant_filtering}{scaffold_length_cutoff})&&($j < $cfg{variant_filtering}{scaffold_number_limit})){
 			$j++;
 		}
 	}
 	close OT;
 
-	$cfg{step1}{variant_filtering}{scaffold_number_limit} = $j;
-	my $scaffold_number_limit = $cfg{step1}{variant_filtering}{scaffold_number_limit};
-
+	$cfg{variant_filtering}{scaffold_number_limit} = $j;
+	my $scaffold_number_limit = $cfg{variant_filtering}{scaffold_number_limit};
+	print "scaffold_number_limit: $scaffold_number_limit\n";
+	
 	#Based on default SNV sites + no singletons + no missing data + minDP 2 + minQ 30  (high quality SNPs)
-	open CL, ">$var{shpath}/cmd_variant_filtering_s3.list";
-	open SH, ">$var{shpath}/variant_filtering_s3.sh";
-	print SH "cd $var{outpath}\n";
-	print SH "vcftools --gzvcf $cfg{step1}{variant_filtering}{vcf} --singletons --stdout >$var{outpath}/singletons.list\n";
-	print SH "vcftools --gzvcf $cfg{step1}{variant_filtering}{vcf} --exclude-positions $var{outpath}/singletons.list --max-missing 1 --max-alleles 2 --minDP 2 --minQ 30 --recode --recode-INFO-all --stdout |bgzip -c >$var{outpath}/high_confidence.vcf.gz\n";
+	open CL, ">$var{shpath}/cmd_variant_filtering_s2.list";
+	open SH, ">$var{shpath}/variant_filtering_s2.sh";
+	print SH "#!/bin/sh\ncd $var{outpath}\n";
+	print SH "vcftools --gzvcf $cfg{variant_filtering}{basic_vcf} --singletons --stdout >$var{outpath}/singletons.list\n";
+	print SH "vcftools --gzvcf $cfg{variant_filtering}{basic_vcf} --exclude-positions $var{outpath}/singletons.list --max-missing 1 --max-alleles 2 --recode --recode-INFO-all --stdout |bgzip -c >$var{outpath}/nosingle_snp_dp.vcf.gz\n";
 
 	#Based on high quality SNV sites + low LD
-	print SH 'bcftools annotate --threads 6 --rename-chrs', " $var{outpath}/chr_map.list" ," $var{outpath}/high_confidence.vcf.gz", '|perl -ne \'if (/#\S+ID=(\d+)/){if($1<=',"$scaffold_number_limit",'){print;}}elsif(/^#/){print;}elsif(/^(\d+)\s+/){if($1<= ',"$scaffold_number_limit",'){print;}}\'|vcftools --vcf - --plink',"\n";
+	print SH 'bcftools annotate --threads $var{threads} --rename-chrs', " $var{outpath}/chr_map.list" ," $var{outpath}/nosingle_snp_dp.vcf.gz", '|perl -ne \'if (/#\S+ID=(\d+)/){if($1<=',"$scaffold_number_limit",'){print;}}elsif(/^#/){print;}elsif(/^(\d+)\s+/){if($1<= ',"$scaffold_number_limit",'){print;}}\'|vcftools --vcf - --plink',"\n";
 	
 	print SH "plink --file out --make-bed --chr-set $scaffold_number_limit no-xy no-mt no-y\n";
-	print SH "plink --bfile plink --indep-pairwise $cfg{step1}{variant_filtering}{ldwindowsize} $cfg{step1}{variant_filtering}{ldwindowstep} $cfg{step1}{variant_filtering}{ldcutoff} --chr-set $scaffold_number_limit no-xy no-mt no-y\n";
-	print SH "plink --bfile plink --extract plink.prune.in --make-bed --out high_confidence_prunned --chr-set $scaffold_number_limit no-xy no-mt no-y\n";
-	print SH "plink --bfile high_confidence_prunned --chr-set $scaffold_number_limit no-xy no-mt no-y --recode vcf --out high_confidence_pre\n";
-	print SH "cat high_confidence_pre.vcf |perl -ne \'print unless (/CHROM/);if(/CHROM/){s/_\\S+//g;print;}\' | bgzip -c >$var{outpath}/high_confidence_prunned.vcf.gz\n";
-
-	print SH "ls $var{outpath}/high_confidence_prunned.vcf.gz && echo \"** finish variant_filtering_s3 **\" > $var{shpath}/variant_filtering_s3.finished.txt";
-	
+	print SH "plink --bfile plink --indep-pairwise $cfg{variant_filtering}{ldwindowsize} $cfg{variant_filtering}{ldwindowstep} $cfg{variant_filtering}{ldcutoff} --chr-set $scaffold_number_limit no-xy no-mt no-y\n";
+	print SH "plink --bfile plink --extract plink.prune.in --make-bed --out nosingle_snp_dp_prunned --chr-set $scaffold_number_limit no-xy no-mt no-y\n";
+	print SH "plink --bfile nosingle_snp_dp_prunned --chr-set $scaffold_number_limit no-xy no-mt no-y --recode vcf --out nosingle_snp_dp_pre\n";
+	print SH "cat $var{outpath}/nosingle_snp_dp_pre.vcf |perl -ne \'print unless (/CHROM/);if(/CHROM/){s/_\\S+//g;print;}\' | bgzip -c >$var{outpath}/nosingle_snp_dp_prunned.vcf.gz\n";
+	print SH "ls $var{outpath}/nosingle_snp_dp_prunned.vcf.gz && echo \"** finish variant_filtering_s2 **\" > $var{shpath}/variant_filtering_s2.finished.txt";
 	close SH;
-
-	print CL "sh $var{shpath}/variant_filtering_s3.sh 1>$var{shpath}/variant_filtering_s3.sh.o 2>$var{shpath}/variant_filtering_s3.sh.e\n";
+	print CL "sh $var{shpath}/variant_filtering_s2.sh 1>$var{shpath}/variant_filtering_s2.sh.o 2>$var{shpath}/variant_filtering_s2.sh.e\n";
 	close CL;
-	`perl $Bin/lib/qsub.pl -d $var{shpath}/variant_filtering_s3_qsub -q $cfg{args}{queue} -P $cfg{args}{prj} -l 'vf=1G,num_proc=2 -binding linear:1' -m 100 -r $var{shpath}/cmd_variant_filtering_s3.list` unless (defined $opts{skipsh});
+	`perl $Bin/lib/qsub.pl -d $var{shpath}/variant_filtering_s2_qsub -q $cfg{args}{queue} -P $cfg{args}{prj} -l 'vf=1G,num_proc=$var{threads} -binding linear:1' -m 100 -r $var{shpath}/cmd_variant_filtering_s2.list` unless (defined $opts{skipsh});
 
-	$cfg{step1}{variant_filtering}{plink_data}="$var{outpath}/high_confidence_prunned";
-	$cfg{step1}{variant_filtering}{high_confidence_vcf}="$var{outpath}/high_confidence.vcf.gz";
-	$cfg{step1}{variant_filtering}{lowld_high_confidence_vcf}="$var{outpath}/high_confidence_prunned.vcf.gz";
+	$cfg{variant_filtering}{plink_data}="$var{outpath}/nosingle_snp_dp_prunned";
+	$cfg{variant_filtering}{high_confidence_vcf}="$var{outpath}/nosingle_snp_dp.vcf.gz";
+	$cfg{variant_filtering}{lowld_high_confidence_vcf}="$var{outpath}/nosingle_snp_dp_prunned.vcf.gz";
 	
 	### format a report
 	my %report;
-	if ($cfg{step1}{variant_filtering}{vcf} =~ /.gz$/) { 
-		open(IN, "gunzip -c $cfg{step1}{variant_filtering}{vcf} |") || die "can’t open pipe to $cfg{step1}{variant_filtering}{vcf}";
+	if ($cfg{variant_filtering}{basic_vcf} =~ /.gz$/) { 
+		open(IN, "gunzip -c $cfg{variant_filtering}{basic_vcf} |") || die "can’t open pipe to $cfg{variant_filtering}{basic_vcf}";
 	} else { 
-		open(IN, $cfg{step1}{variant_filtering}{vcf}) || die "can’t open $cfg{step1}{variant_filtering}{vcf}";
+		open(IN, $cfg{variant_filtering}{basic_vcf}) || die "can’t open $cfg{variant_filtering}{basic_vcf}";
 	}
 
 	$report{snv1}{number}=0;
@@ -265,11 +262,11 @@ sub FinalFiltering {
 	}
 	close IN;
 
-	if ($cfg{step1}{variant_filtering}{lowld_high_confidence_vcf} =~ /.gz$/) {
+	if ($cfg{variant_filtering}{lowld_high_confidence_vcf} =~ /.gz$/) {
 		open(IN, "gunzip -c $cfg{step1}{variant_filtering}{lowld_high_confidence_vcf} |") || die "can’t open pipe to $cfg{step1}{variant_filtering}{lowld_high_confidence_vcf}";
 	}
 	else {
-		open(IN, $cfg{step1}{variant_filtering}{lowld_high_confidence_vcf}) || die "can’t open $cfg{step1}{variant_filtering}{lowld_high_confidence_vcf}";
+		open(IN, $cfg{variant_filtering}{lowld_high_confidence_vcf}) || die "can’t open $cfg{step1}{variant_filtering}{lowld_high_confidence_vcf}";
 	}
 
 	$report{snv3}{number}=0;
